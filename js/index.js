23de235efc7e1f258068b62775cc84b7f01bd1f8
Iury Landin <iurylandin@gmail.com>
2019-11-10 03:52:15 +0000

    Exibir lista de episodios

--- js/index.js
+++ js/index.js
@@ -11,13 +11,32 @@ function animeList() {
         });
 }
 
+function getEpisodio(id) {
+    axios.get(base_url + '/episodio?id='+id, { headers: {'Access-Control-Allow-Origin': '*'} } ).then(function(response) {
+            console.log(response.data);
+            montarTabelaEpisodio(response.data);
+        });
+}
+
 function montarTabelaAnime(data) {
     const list = $('#lista');
     for(i=0; i< data.anime.length; i++){
-        let html = `<div class='anime'> <img onclick="abrirModal('${data.anime[i].Nome}', '${encodeURIComponent(data.anime[i].Desc)}' )" src="${data.anime[i].Imagem}" />
+        let html = `<div class='anime'> <img onclick="abrirModal(${data.anime[i].Id}, '${data.anime[i].Nome}', '${encodeURIComponent(data.anime[i].Desc)}' )" src="${data.anime[i].Imagem}" />
                     <legend>${data.anime[i].Nome}</legend> </div>
                     `;
         $("#lista").append( html )
+    }
+}
+
+function montarTabelaEpisodio(data) {
+    //LIMPAR UL
+    $("#epsLista").html("");
+
+
+     for(i=0; i< data.length; i++){
+        let html = `<li>${data[i].Nome}</li>
+                    `;
+        $("#epsLista").append( html )
     }
 }
 
@@ -26,10 +45,15 @@ function montarTabelaAnime(data) {
 var modal = document.getElementById("modal");
 var span = document.getElementsByClassName("close")[0];
 
-function abrirModal(nome, desc) {
-    $("#desc").html("<h3> "+ nome +" </h3>" + decodeURIComponent(desc) );
+function abrirModal(id, nome, desc) {
+    let HTMLdesc = "<h3> "+ nome +" </h3>" + decodeURIComponent(desc);
+    $("#desc").html( HTMLdesc );
+    getEpisodio(id);
     modal.style.display = "block";
 }
+
+
+
 
 span.onclick = function() {
   modal.style.display = "none";
