729f354d006bbcc99809bc52e9a9a428d626e509
Júlio César <julczar21@outlook.com>
2019-11-09 22:41:53 -0300

    adicionado cdn do Axios
    testes iniciais com o .get do axios

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
-let teste = "Hello World!"
-console.log (teste)
+const link = 'http://cinex.96.lt/animeapi/anime?format=json';
+
+(function () {
+    api()
+})()
+
+function api(){
+    axios
+        .get(link, {timeout: 5000})
+        .then(res => console.log(res.anime[0]))
+        .catch(err => console.log(err))
+}
