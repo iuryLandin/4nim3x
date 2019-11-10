52f5d8a16af2470ca49577afa31c44bc5f875864
Iury Landin <iurylandin@gmail.com>
2019-11-10 02:43:30 +0000

    axios + jquery + 1st front-end preview

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-const link = 'http://cinex.96.lt/animeapi/anime?format=json';
+const link = 'http://cinex.96.lt/animeapi/anime';
+const base_url = 'js/teste.json';
 
-(function () {
-    api()
+
+(function() {
+    animeList()
 })()
 
-function api(){
-    axios
-        .get(link, {timeout: 5000})
-        .then(res => console.log(res.anime[0]))
-        .catch(err => console.log(err))
+function animeList() {
+    axios.get(base_url)
+        .then(function(response) {
+            montarTabelaAnime(response.data);
+        });
+}
+
+function montarTabelaAnime(data) {
+    const list = $('#lista');
+    for(i=0; i< data.anime.length; i++){
+        let html = `<div class='anime'> <img src='${data.anime[i].Imagem}' />
+                    <legend>${data.anime[i].Nome}</legend> </div>
+                    `;
+        $("#lista").append( html )
+    }
+
+
 }
