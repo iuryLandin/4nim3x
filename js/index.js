1495c806d4e17148bb8cc97cab0c494eb65119d0
Iury Landin <iurylandin@gmail.com>
2019-11-10 03:04:49 +0000

    Description modal

--- js/index.js
+++ js/index.js
@@ -16,11 +16,29 @@ function animeList() {
 function montarTabelaAnime(data) {
     const list = $('#lista');
     for(i=0; i< data.anime.length; i++){
-        let html = `<div class='anime'> <img src='${data.anime[i].Imagem}' />
+        let html = `<div class='anime'> <img onclick="abrirModal('${data.anime[i].Nome}', '${encodeURIComponent(data.anime[i].Desc)}' )" src="${data.anime[i].Imagem}" />
                     <legend>${data.anime[i].Nome}</legend> </div>
                     `;
         $("#lista").append( html )
     }
+}
 
 
+
+var modal = document.getElementById("modal");
+var span = document.getElementsByClassName("close")[0];
+
+function abrirModal(nome, desc) {
+    $("#desc").html("<h3> "+ nome +" </h3>" + decodeURIComponent(desc) );
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
