0b0a7b7f015a8abcab38c1c4ca9edd0e70e3e78d
Iury Landin <iurylandin@gmail.com>
2019-11-12 20:56:30 +0000

    loading rename menu.js > utils.js

--- js/menu.js
+++
@@ -1,9 +0,0 @@
- function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.querySelector("main").style.marginLeft = "250px";
-}
-
- function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.querySelector("main").style.marginLeft = "0";
-}
