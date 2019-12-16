8711cf3ce63eb1812792f30c12524f39af0c450b
Iury Landin <iurylandin@gmail.com>
2019-12-16 02:41:31 +0000

    Botao compartilhar 2

--- js/utils.js
+++ js/utils.js
@@ -75,9 +75,7 @@ function mudaPesq(html) {
 
 
 function compartilhar(){
- 
      $('.shareDiv').slideToggle();
-     //$('.shareDiv').show();
 }
 
 function share(rede){
@@ -85,6 +83,6 @@ function share(rede){
     if('fb') url = 'https://www.facebook.com/sharer/sharer.php?u=https://animexonline.herokuapp.com/share.html?id=' + animeId;
     if('wpp') url = 'https://api.whatsapp.com/send?text=Ei,%20assiste%20esse%20anime...%20%0A%20Clica%20no%20link%20%0A%20%0A%20https://animexonline.herokuapp.com/share.html?id=' + animeId
 
-
+    $('.shareDiv').slideToggle();
     window.open(url);
 }
