6ffbaeef98ccf3681e646cf10fc096dcce20705f
JulCzar <54212199+JulCzar@users.noreply.github.com>
2020-04-20 20:38:20 -0300

    update Api

--- js/utils/endpoints.js
+++ js/utils/endpoints.js
@@ -1,16 +1,12 @@
 //Configuração referentes à página e os links que irão ser carregados
 //Estas staticantes contêm os EndPoints usados na API
 const Endp = {
-    qgElet: 'https://qgeletronicos.com/animeapi', //link qgEletronicos
-    cinex : 'http://cinex.96.lt/animeapi',        //link cinex
-    anime : '/anime?next=',    //lista de animes em ordem alfabética
+    anime : '/animes?next=',    //lista de animes em ordem alfabética
     categ : '/categoria',      //endpoint das categorias para seleção
     video : '/video?id=',      //endpoint para acesso aos videos
     lanca : '/lancamento',     //lista de animes da temporada
     episo : '/episodio?id=',   //endpoint para acesso aos episodios
-    baseUrl() {
-        return this.qgElet //API
-    },
+    baseUrl: 'https://animexapi.herokuapp.com/'
 }
 
 function safeImg(imgURL) {
@@ -18,7 +14,7 @@ function safeImg(imgURL) {
 }
 
 function getApiLink(endpoint) {
-    return `${Endp.baseUrl() + endpoint}`       //função que devolve os links prontos para uso
+    return `${Endp.baseUrl + endpoint}`       //função que devolve os links prontos para uso
 }
 
 // Cabeçalho usado no axios para obter as requisições
